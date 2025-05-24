15b040cfabb52a344b45ff2ddf271e96e0cb37e2
YaBoyHasan <104626312+YaBoyHasan@users.noreply.github.com>
2025-05-24 23:32:43 +0100

    scraper, schedule API polling

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from app.routes.core import core_blueprint
 from app.routes.auth import auth_blueprint
 from app.models.user import db, User
+from apscheduler.schedulers.background import BackgroundScheduler
+from app.services.scraper import fetch_and_store_comp_data  # adjust path if needed
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -23,5 +26,16 @@ def create_app():
 
     app.register_blueprint(core_blueprint)
     app.register_blueprint(auth_blueprint)
+
+   # APScheduler setup
+    if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        scheduler = BackgroundScheduler()
+
+        def scheduled_job():
+            with app.app_context():
+                fetch_and_store_comp_data()
+
+        scheduler.add_job(func=scheduled_job, trigger="interval", hours=1)
+        scheduler.start()
 
     return app
