8d2e7b1574d675cee07d05f2560c94e91cefa3e6
YaBoyHasan <104626312+YaBoyHasan@users.noreply.github.com>
2025-05-24 13:48:00 +0100

    tester123

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,25 @@
-from flask import Flask
+from flask import Flask, g, session
 from config import Config
 from app.core.routes import core_blueprint
 from app.auth.routes import auth_blueprint
-from app.models.user import db
+from app.models.user import db, User
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    
+
     db.init_app(app)
 
     with app.app_context():
         db.create_all()
+
+    @app.before_request
+    def load_logged_in_user():
+        user_id = session.get("user_id")
+        if user_id:
+            g.user = User.query.get(user_id)
+        else:
+            g.user = None
 
     app.register_blueprint(core_blueprint)
     app.register_blueprint(auth_blueprint)
