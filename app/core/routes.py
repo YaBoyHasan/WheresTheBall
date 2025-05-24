9241a828d68f183ae67e94e4abcf81f742644191
YaBoyHasan <104626312+YaBoyHasan@users.noreply.github.com>
2025-05-24 12:02:58 +0100

    structure improvements

--- app/core/routes.py
+++ app/core/routes.py
@@ -11,16 +11,16 @@ latest_prediction = {
 
 @core_blueprint.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("core/home.html")
 
 @core_blueprint.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("core/about.html")
 
 @core_blueprint.route("/past")
 def past_predictions():
-    return render_template("past.html")
+    return render_template("core/past.html")
 
 @core_blueprint.route("/latest")
 def latest_prediction():
-    return render_template("latest.html", latest=latest_prediction)
+    return render_template("core/latest.html", latest=latest_prediction)
